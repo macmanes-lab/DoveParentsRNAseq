46054e50e9606543cea265e70e3a9f0de81c1e83
raynamharris <rayna.harris@utexas.edu>
2019-04-02 13:06:54 -0700

    add files for binerization

---
+++ install.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("kableExtra")
+install.packages("cowplot")
+
+source("http://www.bioconductor.org/biocLite.R")
+biocLite("edgeR")
+biocLite("Glimma")
+biocLite("limma")
