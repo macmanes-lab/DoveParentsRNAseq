c99128a7fbdcf148f58181519dab42c6f0e22311
raynamharris <rayna.harris@utexas.edu>
2019-05-12 20:12:05 -0700

    bioc

--- install.R
+++ install.R
@@ -1,9 +1,12 @@
-pkgs = c("BiocManager",
-         "tidyverse", "knitr", "rmarkdown",
+pkgs = c("tidyverse", "knitr", "rmarkdown",
          "kableExtra", "cowplot", "RColorBrewer",
          "pheatmap", "viridis" )
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
+
+if (!requireNamespace("BiocManager"))
+  install.packages("BiocManager")
+BiocManager::install()
 
 BiocManager::install("edgeR")
 BiocManager::install("Glimma")
