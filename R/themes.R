afc1a93672f16436265c3ca1d08e17ff6befe27c
raynamharris <rayna.harris@utexas.edu>
2019-05-23 23:39:36 -0700

    new colors new themee

---
+++ R/themes.R
@@ -0,0 +1,26 @@
+# custom themes for plots
+
+# hex colors for last full day of parents life
+colorlastday <-  c("control" = "#77787A", 
+                   "nest building" = "#DA733E", 
+                   "eggs lay" = "##559FD9",
+                   "eggs early" = "#9AD0C3",
+                   "eggs middle" = "#68C2A8", 
+                   "eggs later" = "#37A589",  
+                   "eggs delay" = "#19977D",
+                   "chicks hatch" = "#BAB5D2",   
+                   "chicks early" = "#9993C3", 
+                   "chicks later" = "#7874B0",
+                   "empty nest" = "#754E27")
+
+# hex colors for last 2nd to last day of parents life
+colorpenultimate <- c("control" = "#77787A", 
+                      "nest building" = "#DA733E", 
+                      "eggs early" = "#9AD0C3",
+                      "eggs middle" = "#68C2A8", 
+                      "eggs later" = "#37A589",  
+                      "eggs delay" = "#19977D",
+                      "chicks hatch" = "#BAB5D2",   
+                      "chicks early" = "#9993C3", 
+                      "chicks later" = "#7874B0",
+                      "empty nest" = "#754E27")
