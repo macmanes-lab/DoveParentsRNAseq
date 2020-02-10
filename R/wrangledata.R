d02327ac9a3d2c7340ae4c889b9c703a3bd171a4
raynamharris <rayna.harris@utexas.edu>
2020-02-10 14:26:02 -0800

    better fig 1 and 3

--- R/wrangledata.R
+++ R/wrangledata.R
@@ -1,4 +1,4 @@
-# data wrangle for figure 1
+# data wrangle for multiple figures
 
 # Note, the input file is too big for storage on github :(
 
@@ -18,34 +18,13 @@ colData <- colData %>% mutate(tissue = fct_recode(tissue, "gonads" = "gonad"))
 colData$tissue <- factor(colData$tissue, levels = tissuelevels)
 row.names(colData) <- colData$V1
 
+# sample data
+colData <- read.csv("../metadata/00_samples.csv", header = T, row.names = 1)
+colData$treatment <- factor(colData$treatment, levels = alllevels)
+colData <- colData %>% mutate(tissue = fct_recode(tissue, "gonads" = "gonad"))
+colData$tissue <- factor(colData$tissue, levels = tissuelevels)
+row.names(colData) <- colData$V1
+
 # check ready for analysis
 #row.names(countData) == row.names(colData)
-
-
-## PRL vsd
-
-vsd_path <- "../results/DEseq2/"   # path to the data
-vsd_files <- dir(vsd_path, pattern = "*vsd.csv") # get file names
-vsd_pathfiles <- paste0(vsd_path, vsd_files)
-vsd_files
-
-PRLvsd <- vsd_pathfiles %>%
-  setNames(nm = .) %>% 
-  map_df(~read_csv(.x), .id = "file_name")  %>% 
-  dplyr::rename("gene" = "X1") %>% 
-  pivot_longer(cols = L.G118_female_gonad_control:y98.o50.x_male_pituitary_inc.d3, 
-               names_to = "samples", values_to = "counts") %>%
-  filter(gene == "PRL")  %>%
-  dplyr::mutate(sextissue = sapply(strsplit(file_name, '_vsd.csv'), "[", 1)) %>%
-  dplyr::mutate(sextissue = sapply(strsplit(sextissue, '../results/DEseq2/'), "[", 2)) %>%
-  dplyr::mutate(sex = sapply(strsplit(sextissue, '\\_'), "[", 1),
-                tissue = sapply(strsplit(sextissue, '\\_'), "[", 2),
-                treatment = sapply(strsplit(samples, '\\_'), "[", 4)) %>%
-  dplyr::mutate(treatment = sapply(strsplit(treatment, '.NYNO'), "[", 1)) %>%
-  dplyr::select(sex, tissue, treatment, gene, samples, counts)
-PRLvsd$treatment <- factor(PRLvsd$treatment, levels = alllevels)
-
-PRLhyp <- PRLvsd %>% filter(tissue == "hypothalamus")
-PRLpit <- PRLvsd %>% filter(tissue == "pituitary")
-PRLgon <- PRLvsd %>% filter(tissue == "gonad")
 
