17b278f7dea5d226a06e7519dca20b630e4c288d
raynamharris <rayna.harris@utexas.edu>
2020-02-09 18:54:32 -0800

    suppl fig

---
+++ R/wrangledata.R
@@ -0,0 +1,24 @@
+# data wrangle
+
+# Note, the input file is too big for storage on github :(
+
+pseudocounts <- read_csv("../results/01_pseudo.counts.csv")
+head(pseudocounts[1:3])
+pseudocounts <- as.data.frame(pseudocounts)
+row.names(pseudocounts) <- pseudocounts$X1
+pseudocounts$X1 <- NULL
+# prep count data for all samples
+countData <- as.data.frame(t(pseudocounts))
+head(countData[1:3])
+
+# prep col data for all samples
+colData <- read.csv("../metadata/00_samples.csv", header = T, row.names = 1)
+colData$treatment <- factor(colData$treatment, levels = alllevels)
+colData <- colData %>% mutate(tissue = fct_recode(tissue, "gonads" = "gonad"))
+colData$tissue <- factor(colData$tissue, levels = tissuelevels)
+row.names(colData) <- colData$V1
+
+# check ready for analysis
+#row.names(countData) == row.names(colData)
+
+
