453f4f39c8f735bd3c3dd72262d826cce3880a2c
Pachamaltese <mvargas@dcc.uchile.cl>
2020-03-04 16:26:20 -0300

    DO + PSQL

---
+++ db-export/01-test-db.R
@@ -0,0 +1,15 @@
+library(RPostgreSQL)
+library(dplyr)
+library(dbplyr)
+
+con <- dbConnect(
+  dbDriver("PostgreSQL"),
+  host = Sys.getenv("psql_rayna_ip"),
+  port = Sys.getenv("psql_rayna_port"),
+  user = Sys.getenv("psql_rayna_usr"),
+  password = Sys.getenv("psql_rayna_pwd"),
+  dbname = Sys.getenv("psql_rayna_db")
+)
+
+tbl(con, in_schema("public", "alldeg")) %>% 
+  filter(gene == "AOC1")
