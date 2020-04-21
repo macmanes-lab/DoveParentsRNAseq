b13b40182b1031fc4050591d095e885fa15d1355
raynamharris <rayna.harris@utexas.edu>
2020-04-21 12:04:22 -0700

    now with candidate genes in wangle data, finally

--- R/wrangledata.R
+++ R/wrangledata.R
@@ -1,6 +1,6 @@
 # data wrangle for multiple figures
 
-# this is for tsne and pca
+#### this is for tsne and pca
 # Note, pseudocounts is too big for storage on github :(
 pseudocounts <- read_csv("../results/01_pseudo.counts.csv")
 head(pseudocounts[1:3])
@@ -21,3 +21,61 @@ row.names(colData) <- colData$V1
 
 # check ready for analysis
 #row.names(countData) == row.names(colData)
+
+
+#### candidate gene anlayses
+
+# all genes in this parental care dataset 
+geneids <- read_csv("../metadata/00_geneinfo.csv") %>%
+  select(-X1) %>%
+  rename("NCBI" = "entrezid",
+         "gene" = "Name")
+
+# genes from Ch 17 Evolution of parental care
+curleychampagnegenes <- c("AVPR1A", "ADRA2A",    
+                          "COMT", "CRH", "CRHBP", "CRHR1", "CRHR2", 
+                          "DRD1", "DRD4","ESR1", "ESR2", #ERa ERbeta
+                          "FOS", "HTR2C", "MEST", "NR3C1", #GR
+                          "OPRM1", "PGR", "PRL", "PRLR",  "SLC6A4") #5HTT
+curleychampagnegenes <- as.data.frame(curleychampagnegenes) %>%
+  mutate(GO = "parentalcare", gene = curleychampagnegenes)  %>%
+  select(GO, gene)
+curleychampagnegenes
+
+
+# candidate gens from parental care GO terms 
+GO_path <- "../metadata/goterms/"   # path to the data
+GO_files <- dir(GO_path, pattern = "*.txt") # get file names
+GO_pathfiles <- paste0(GO_path, GO_files)
+
+GOgenesLong <- GO_pathfiles %>%
+  setNames(nm = .) %>% 
+  map_df(~read_table(.x, col_types = cols(), col_names = FALSE), .id = "file_name") %>% 
+  mutate(GO = sapply(strsplit(as.character(file_name),'../metadata/goterms/'), "[", 2)) %>% 
+  mutate(GO = sapply(strsplit(as.character(GO),'.txt'), "[", 1)) %>% 
+  mutate(gene = sapply(strsplit(as.character(X1), "[\\\\]|[^[:print:]]" ), "[", 2)) %>% 
+  select(GO, gene)  %>%
+  filter(gene != "Symbol") %>%
+  distinct(GO,gene)  %>%
+  mutate(gene = toupper(gene)) %>%
+  rbind(., curleychampagnegenes) %>%
+  arrange(gene) %>%
+  left_join(., geneids, by = "gene") %>%
+  drop_na() 
+GOgenesLong
+
+GOgenesWide <- GOgenesLong %>% 
+  pivot_wider(
+    names_from = GO,
+    values_from = gene) %>%
+  mutate(numGOs = 4 - rowSums(is.na(.))) %>%
+  arrange(desc(numGOs)) %>%
+  select(-numGOs) %>%
+  left_join(geneids, by = c("geneid","NCBI"))
+GOgenesWide
+
+candidategenes <- GOgenesLong %>% distinct(gene) %>% pull(gene)
+
+# note: I can't find these genes in dataset: NOS1 or OXTR or FOX1B or HTR5A
+
+
