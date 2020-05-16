17404c38bd42ec569cf23c7dc97e2966a047771e
raynamharris <rayna.harris@utexas.edu>
2020-05-16 11:57:29 -0700

    name cleaning

--- analysis/old/nests.R
+++ analysis/old/nests.R
@@ -4,7 +4,6 @@ library(tidyverse)
 
 # read bird data
 birds <- read_csv("../../metadata/00_birds.csv") %>% select(-X1)
-head(birds)
 
 #wrangle nest data
 neststemp1 <- read_csv("../../metadata/nestsummary.csv") %>%
@@ -12,11 +11,15 @@ neststemp1 <- read_csv("../../metadata/nestsummary.csv") %>%
   drop_na()  %>%
   dplyr::rename("bird" = "AdultId_1BandNum",
                 "nestmate" = "AdultId_2BandNum") %>%
-  mutate(pair = row.names(.),
+  mutate(pair = as.numeric(row.names(.)),
          bird = gsub("-", ".", bird),
          bird = gsub("/", ".", bird),
+         bird = sapply(strsplit(bird, ' '), "[", 1),
+         bird = sapply(strsplit(bird, '\\('), "[", 1),
          nestmate = gsub("-", ".", nestmate),
-         nestmate = gsub("/", ".", nestmate)) 
+         nestmate = gsub("/", ".", nestmate),
+         nestmate = sapply(strsplit(nestmate, ' '), "[", 1),
+         nestmate = sapply(strsplit(nestmate, '\\C'), "[", 1)) 
 
 neststemp2 <- neststemp1 %>%
   select(nestmate, bird, pair)
@@ -26,7 +29,8 @@ nests <- rbind(neststemp1, neststemp2)
 
 ## join to get paired nests
 
-nestpairs <- left_join(birds, nests, by = "bird")
+nestpairs <- left_join(birds, nests, by = "bird") %>%
+  arrange(treatment, pair)
 head(nestpairs)
 
 write.csv(nestpairs, "../../metadata/nestpairs.csv", row.names = F)
