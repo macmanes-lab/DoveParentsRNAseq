59c014af5745cedbd0e045407744a3188368a817
raynamharris <rayna.harris@utexas.edu>
2020-10-30 10:11:02 -0700

    more genes and hormones

--- analysis/07_genesnhormones.R
+++ analysis/07_genesnhormones.R
@@ -25,38 +25,37 @@ hormones <- read_csv("results/AllHorm_02042020_nomiss.csv") %>%
   mutate(e2t = ifelse(sex == "f", e2, t)) %>%
   mutate(sex = recode(sex, "f" = "female", "m" = "male" )) %>%
   select(id, treatment, sex, prl, cort, p4, e2t)  
-glimpse(hormones)
+head(hormones)
 
 # vsds (gene expression) ----
-pitvsdAll <- read_csv("results/03_pitvsdAll.csv")
-hypvsdAll <- read_csv("results/03_hypvsdAll.csv")
-gonvsdAll <- read_csv("results/03_gonvsdAll.csv")
+
+
+vsd_path <- "results/"   # path to the data
+vsd_files <- c("03_gonvsdf.csv" ,  "03_gonvsdm.csv" ,
+               "03_hypvsdm.csv" ,   "03_hypvsdf.csv",
+               "03_pitvsdf.csv" ,     "03_pitvsdm.csv" )
+vsd_pathfiles <- paste0(vsd_path, vsd_files)
+vsd_pathfiles
+
+candidatevsds <- vsd_pathfiles %>%
+  map_dfr(~read_csv(.x)) %>%
+  mutate(id = sapply(strsplit(as.character(samples),'\\_'), "[", 1)) %>%
+  filter(id != "x.g.g.ATLAS") %>%
+  select(id, tissue, gene, counts) %>%
+  pivot_wider(names_from = "gene", values_from = "counts")
+head(candidatevsds)
 
 # join genes and hormones ----
-genesnhomrmones <- function(vsds, whichsex){
-  df <-  vsds %>% 
-    inner_join(hormones, ., by = "id") %>%
-    filter(sex %in% whichsex) 
-  print(head(df)[1:10])
-  return(df)
-}
-
-pitf <- genesnhomrmones(pitvsdAll, "female")
-gonf <- genesnhomrmones(hypvsdAll, "female")
-hypf <- genesnhomrmones(gonvsdAll, "female") 
-
-pitm <- genesnhomrmones(pitvsdAll, "male")
-gonm <- genesnhomrmones(hypvsdAll, "male")
-hypm <- genesnhomrmones(gonvsdAll, "male") 
-
-pit <- genesnhomrmones(pitvsdAll, c("female", "male"))
-gon <- genesnhomrmones(hypvsdAll, c("female", "male"))
-hyp <- genesnhomrmones(gonvsdAll, c("female", "male")) 
+genesnhomrmones <- inner_join(hormones, candidatevsds, by = "id")  %>%
+  select(id, sex, tissue, treatment, 
+         prl, cort, p4, e2t, everything()) %>%
+  mutate(tissue = factor(tissue, levels = tissuelevels))
+head(genesnhomrmones)
 
 # correlations ---
 
-plotcorrelation <- function(df, xvar, yvar, xlab, ylab){
-  df %>%
+plotcorrelation <- function(xvar, xlab, yvar, ylab){
+  p <- genesnhomrmones %>%
     ggplot(aes(x = xvar, y = yvar)) +
     geom_point(aes(color = treatment)) +
     geom_smooth(method = "lm", aes(color = sex)) +
@@ -64,94 +63,63 @@ plotcorrelation <- function(df, xvar, yvar, xlab, ylab){
     scale_x_log10() +
     labs(x = xlab, y = ylab) +
     scale_color_manual(values = allcolors) +
-    theme(legend.position = "none") +
-    facet_wrap(~sex)
+    facet_wrap(~tissue, ncol = 1, scales = "free", switch = T) +
+    theme(legend.position = "none")
+  p
 }
 
 
-a <- plotcorrelation(hyp,hyp$PRL, hyp$prl, 
-                     "Hypothalamic prolactin gene expression", 
-                     " ")
+plotcorrelation(genesnhomrmones$e2t, "Circulating E2 or T (ng/mL)",
+                genesnhomrmones$PRLH, "PRLH expression")
 
-b <- plotcorrelation(pit,pit$PRL, pit$prl, 
-                     "Pituitary prolactin gene expression", 
-                     "Circulating prolactin (ng/mL)") +
-  theme(strip.text = element_blank())
+plotcorrelation(genesnhomrmones$prl, "Circulating prolactin (ng/mL)",
+                genesnhomrmones$PRL, "Prolactin (PRL) expression")
 
-c <- plotcorrelation(gon,gon$PRL, gon$prl, 
-                     "Gonadal prolactin gene expression", 
-                     " ") +
-  theme(strip.text = element_blank())
+plotcorrelation(genesnhomrmones$prl, "Circulating prolactin (ng/mL)",
+                genesnhomrmones$PRL, "Prolactin (PRL) expression")
 
 plot_grid(a,b,c, ncol = 1)
 
 
 
-calculatecorrs <- function(df){
-  df2 <- df %>%
-    select(-id, -sex, -treatment) %>%
-    correlate(.)
-  print(head(df2)[1:6])
-  return(df2)
-}
-
-
-hypfcorrs <- calculatecorrs(hypf)
-hypmcorrs <- calculatecorrs(hypm)
-
-pitfcorrs <- calculatecorrs(pitf)
-pitmcorrs <- calculatecorrs(pitm)
-
-gonfcorrs <- calculatecorrs(gonf)
-gonmcorrs <- calculatecorrs(gonm)
-
-gettop10PRL <- function(df){
-  
-  top5 <- df %>% focus(PRL) %>% arrange(desc(PRL)) %>% head(.,5)
-  bottom5 <- df %>% focus(PRL) %>% arrange(PRL) %>% head(.,5)
-  top10PRL <- rbind(top5, bottom5) 
-  return(top10PRL)
-}
-
-a <- gettop10PRL(pitfcorrs) %>% rename("female" = rowname)
-b <- gettop10PRL(pitmcorrs) %>% rename("male" = rowname)
-cbind(a,b)
-
-
-# candidate gene correlations
-geneshormforcorr <- c(candidategenes, "prl", "cort", "p4", "e2t")
-geneshormforcorr <- geneshormforcorr[! geneshormforcorr %in% 
-                                       c("MC3R", "SERPINA4", "VIP")]
-
-
-plotcorrrs <- function(df){
-  p <- df %>% 
-    filter(rowname %in% geneshormforcorr) %>%
-    select(rowname, geneshormforcorr) %>% 
+plotcorrrs <- function(whichtissue, whichsex, mytitle){
+  p <- genesnhomrmones %>%
+    filter(tissue == whichtissue,
+           sex %in% whichsex) %>%
+    select(-id, -sex, -tissue,-treatment) %>%
+    correlate(.) %>% 
+    select(rowname, prl, cort, p4, e2t) %>%
+    filter(!rowname %in% c("prl", "cort", "p4", "e2t")) %>%
     rplot(colors = 
-            c("skyblue1", "white","indianred2"),
-          shape = 1) +
+            c("skyblue1", "white","indianred2")) +
     theme_minimal(base_size = 9) +
-    geom_point(size = 2) +
-    theme(axis.text.x = element_text(angle = 90,
-                                     face = "italic"),
-          legend.position = "none") 
+    theme(axis.text.y = element_blank(), 
+          legend.position = "none") +
+    labs(subtitle = mytitle)
   return(p)
 }
 
+a <- plotcorrrs("hypothalamus", sexlevels, "Hypothalamus") + 
+  theme(axis.text.y = element_text(face = "italic"))
+b <- plotcorrrs("pituitary", sexlevels, "Pituitary")
+c <- plotcorrrs("gonads", sexlevels, "Gonads") + 
+  theme(legend.position = "right")
 
-a <- plotcorrrs(hypfcorrs)
-b <- plotcorrrs(hypmcorrs)
-c <- plotcorrrs(pitfcorrs)
-d <- plotcorrrs(pitmcorrs)
-e <- plotcorrrs(gonfcorrs)
-f <- plotcorrrs(gonmcorrs)
-
-p <- plot_grid(a,b,c,d,e,f, ncol = 2)
+p <- plot_grid(a,b,c, nrow = 1, rel_widths = c(1.2,1,1.2))
 p
 
-png("~/Desktop/corrs.png", width = 10, height = 12, 
-    units = 'in', res = 300)
-plot(p) 
-dev.off()
+e <- plotcorrrs("hypothalamus", "female", "F Hyp") + 
+  theme(axis.text.y = element_text(face = "italic"))
+f <- plotcorrrs("pituitary", "female", "F Pit")
+g <- plotcorrrs("gonads", "female", "F Gon") 
+
+h <- plotcorrrs("hypothalamus", "male", "M Hyp") 
+i <- plotcorrrs("pituitary", "male", "M Pit")
+j <- plotcorrrs("gonads", "male", "M Gon") + 
+  theme(legend.position = "right") 
+p2 <- plot_grid(e,h,f,i,g,j, nrow = 1, rel_widths = c(1.2,1,1,1,1,1,1.2))
+p2
+
+
+
 
