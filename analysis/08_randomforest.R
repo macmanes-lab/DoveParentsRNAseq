ee7656ec23afe596fd07f0f20422db3084b08bd9
raynamharris <rayna.harris@utexas.edu>
2020-11-19 15:49:48 -0800

    now with top DEGs

--- analysis/08_randomforest.R
+++ analysis/08_randomforest.R
@@ -1,4 +1,3 @@
-
 library(tidyverse)
 library(randomForest)
 library(caret)
@@ -11,8 +10,7 @@ source("R/themes.R")
 
 # need bird info to join with vsds 
 
-
-birds <- read_csv("metadata/00_birds_fixed.csv") %>%
+birds <- read_csv("metadata/00_birds.csv") %>%
   mutate(external = fct_collapse(treatment,
                                  eggs = c("lay", "inc.d3", "inc.d9", "inc.d17", "prolong"),
                                  chicks = c("hatch", "n5", "n9", "extend" , "early" ),
@@ -34,17 +32,30 @@ birds <- read_csv("metadata/00_birds_fixed.csv") %>%
                               char = c("lay", "inc.d3", "inc.d9", "hatch", "n5", "n9", "inc.d17" ),
                               manip = c( "m.inc.d3",  "m.inc.d9", "m.inc.d17",  "m.n2",
                                          "early", "extend" ,  "prolong"),
-                              controls = c("control", "bldg")))
+                              controls = c("control", "bldg")),
+         id = tolower(bird),
+         id = sapply(strsplit(id,'\\.atlas'), "[", 1)) %>%
+  select(id, sex, treatment, external, internal, hybrid, study) %>%
+  mutate(treatment = ifelse(grepl("y128.g23.x|blu108.w40.o158|x.blu43.g132|r37.w100.x", 
+                                         id), "m.inc.d9", treatment))
 
-# now, get vsd for only DEGs
+# now, get vsd for only DEGs that have human orthologs
+
+hugo <- read_csv("../musicalgenes/data/hugo.csv") %>% 
+  distinct() %>%
+  pull(gene) 
 
 DEGlist <- read_csv("results/04_allDEG.csv") %>%
-  filter(!grepl("control", comparison)) %>%
-  select(gene) %>% arrange(gene) %>%
-  filter(!grepl("LOC|\\.", gene)) %>%
-  distinct(gene) %>% pull(gene)
+  filter(!grepl("LOC|\\.|\\-", gene),
+         gene %in% hugo) %>%
+  arrange(padj) 
 
-slimDEGs <- DEGlist[5000:6000]
+slimDEGs <- DEGlist %>%
+  filter(!grepl("control|bldg", comparison)) %>%
+  head(., 1490) %>%
+  select(gene) %>%  arrange(gene) %>%
+  distinct(gene) %>% pull(gene) 
+slimDEGs
 
 # select which genes to look at
 savecols <- c("id", "group", slimDEGs)
@@ -60,9 +71,10 @@ DEGvsds <- vsd_pathfiles %>%
 DEGvsds2 <- DEGvsds %>%
   dplyr::select(one_of(savecols))  %>%
   left_join(birds, ., by = "id") %>%
+  filter(id != "r.r.x.r2xr") %>%
   mutate(group = factor(group),
-         treatment = factor(treatment, levels = alllevels)) %>%
-  mutate(tissue = fct_recode(group, "hypothathalamus" = "1",
+         treatment = factor(treatment, levels = alllevels),
+         tissue = fct_recode(group, "hypothathalamus" = "1",
                               "pituitary" = "2",
                               "gonads" = "3")) %>%
   drop_na(tissue) %>% 
@@ -71,6 +83,7 @@ DEGvsds2 <- DEGvsds %>%
   select_if(~ !any(is.na(.))) %>%
   mutate_if(is.character, as.factor) 
 head(DEGvsds2) 
+
 
 
 sample = sample.split(DEGvsds2$treatment, SplitRatio = .75)
@@ -84,32 +97,28 @@ rf1 <- randomForest(
   data=train
 )
 
-rf1 # OOB estimate of  error rate: candidate genes 40.25%, Pgenes 35.22%
+rf1 # OOB estimate of  error rate: candidate genes 40.25%, Pgenes 35.22%, DEGs 30%
 pred = predict(rf1, newdata=test[-1])
 res1 <- test %>%
   mutate(pred = pred) %>%
   select(treatment, pred)
-res1
 
 rf2 <- randomForest(
   external ~ . * sex * tissue,
   data=train
 )
 
-rf2 # OOB estimate of  error rate: candidates: 1.24%, Pgens 2.03%
+rf2 # OOB estimate of  error rate: candidates: 1.24%, Pgens 2.03%, DEGs 2%
 pred2 = predict(rf2, newdata=test[-2])
 res2 <- test %>%
   mutate(pred = pred2) %>%
   select(external, pred)
-res2
-
 
 rf3 <- randomForest(
   internal ~ . * sex * tissue,
   data=train
 )
-
-rf3  # OOB estimate of  error rate: candidates: 1.24%, Pgenes 0%
+rf3  # OOB estimate of  error rate: candidates: 1.24%, Pgenes 0%, DEGs 0.94%
 
 pred3 = predict(rf3, newdata=test[-3])
 res3 <- test %>%
@@ -129,7 +138,7 @@ pred4 = predict(rf4, newdata=test[-4])
 res4 <- test %>%
   mutate(pred = pred4) %>%
   select(hybrid, pred)
-res4
+
 
 rf5 <- randomForest(
   study ~ . * sex * tissue,
@@ -141,7 +150,7 @@ pred5 = predict(rf5, newdata=test[-5])
 res5 <- test %>%
   mutate(pred = pred5) %>%
   select(study, pred)
-res5
+
 
 a <- ggplot(res1, aes(x = treatment, fill = pred)) +
   geom_bar(position = "fill") +
@@ -150,17 +159,16 @@ a <- ggplot(res1, aes(x = treatment, fill = pred)) +
   scale_fill_manual(values = allcolors) +
   labs(y = "percent predicted", 
        x = "parental stage or treatment",
-       subtitle = "OOB estimate of  error rate: 40.25%",
+       subtitle = "OOB estimate of  error rate: 30.08%",
        title = "Prediction based off 75/25 training/testing split") +
   expand_limits(y = 0)
-
 
 b <- ggplot(res2, aes(x = external, fill = pred)) +
   geom_bar(position = "fill") +
   theme_B3() +
   scale_fill_manual(values = colorhypothesis) +
   labs(y = "percent predicted", x = "external stimuli",
-       subtitle = "OOB estimate of  error rate: 2.03%") +
+       subtitle = "OOB estimate of  error rate: 2.14%") +
   expand_limits(y = 0)
 
 c <- ggplot(res3, aes(x = internal, fill = pred)) +
@@ -169,9 +177,8 @@ c <- ggplot(res3, aes(x = internal, fill = pred)) +
   scale_fill_manual(values = colorhypothesis) +
   labs(y = "percent predicted", 
        x = "timing in parental care cycle",
-       subtitle = "OOB estimate of  error rate: 1.24%") +
+       subtitle = "OOB estimate of  error rate: 0.94%") +
   expand_limits(y = 0)
-
 
 e <- ggplot(res5, aes(x = study, fill = pred)) +
   geom_bar(position = "fill") +
@@ -179,7 +186,7 @@ e <- ggplot(res5, aes(x = study, fill = pred)) +
   #scale_fill_manual(values = colorhypothesis) +
   labs(y = "percent predicted", 
        x = "characterization versus manipulation",
-       subtitle = "OOB estimate of  error rate: 5.22") +
+       subtitle = "OOB estimate of  error rate: 4.41%") +
   expand_limits(y = 0)
 
 d <- ggplot(res4, aes(x = hybrid, fill = pred)) +
@@ -188,7 +195,7 @@ d <- ggplot(res4, aes(x = hybrid, fill = pred)) +
   #scale_fill_manual(values = colorhypothesis) +
   labs(y = "percent predicted", 
        x = "hybrid model internal and external",
-       subtitle = "OOB estimate of  error rate: 0.72%") +
+       subtitle = "OOB estimate of  error rate: 2.01%") +
   expand_limits(y = 0)
 
 bc <- plot_grid(b,c)
@@ -196,11 +203,11 @@ de <- plot_grid(e,d)
 abc <- plot_grid(a,bc,de, nrow = 3,
                  rel_heights = c(1.2,1,1))
 
-pdf(file="figures/fig-randomforest.pdf", width=7, height=6)
+pdf(file="figures/fig-randomforest-v2.pdf", width=7, height=6)
 plot(abc)
 dev.off()
 
-png("figures/fig-randomforest.png", width = 7, height = 6, 
+png("figures/fig-randomforest-v2.png", width = 7, height = 6, 
     units = 'in', res = 300)
 plot(abc) 
 dev.off()
